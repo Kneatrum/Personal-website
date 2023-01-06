37d764e2f0aa4535c4c7c911f8af4d7ff8bf140b
martin_mwiti <martin@ariyafinergy.com>
2023-01-06 21:31:04 +0300

    More improvements especially on image displaying

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,16 +8,30 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [{
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `notes`,
-      path: `${__dirname}/src/pages/`,
+  plugins: [
+    'gatsby-transformer-remark',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp', // Needed for dynamic images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
     },
-  },],
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+],
   siteMetadata: {
     title: 'Kneatrum',
     description: 'Hardware Dev Portfolio',
-    copyright: 'This website is copyright 2023 Knreatrum'
+    copyright: 'This website is copyright 2023 Knreatrum',
+    contact: 'martin@kneatrum.com'
   }
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -6,9 +6,16 @@ export default function about() {
     <Layout>
       <div>
           <h1>About Page</h1>
-          <p>Lorem Ipsum Mathogothanio</p>
-          <p>Lorem Ipsum Mathogothanio</p>
-          <p>Lorem Ipsum Mathogothanio</p>
+          <p>A boy was born many years ago and like most young boys he had a dream of becoming a pilot.  He  used to be very active and brilliant.  He recalls an instance where he eavesdropped a conversation between two of his teachers and one was describing him to be very clever. He couldn’t wait to get home and ask his mother what it meant by being clever.</p>
+          <p>As he grew up he  learnt about several things among which were Engineers,  Scientists and  words like far-fetched.  He learnt that were supposed to know a lot of mathematics and since he knew he was poor in math, he didn’t take the word engineer as a title so seriously.  Somehow he also realized that the word far-fetched and pilot would fit in one sentence and make a lot of sense.</p>
+          <p>However things were good on the side of science.  It was his favorite lesson in class and that’s how he learnt about the word scientist. He wanted to be one.</p>
+          <p>Life went on and as usual it can’t be entirely about your thoughts, there had to be some opinions.  Some of his friends told him that he would make a very good priest, for funny reasons he’d say, because he had never preached to deserve such a compliment (a story for another decade)  😛 </p>
+          <p>As he grew up, a lot of things changed apart from his interest in science. Soon he was out of primary school,  into high school and he says that the only thing he enjoyed there were his science lessons,  his weird choice of books in their library and sleep. </p>
+          <p>Some of his friends would regularly ask him why he used to read material that would never appear on an examination paper and he’d offer a smile for an answer.  His performance in class was always slightly above and below average and though it was disturbing,  he always found a way to push on. He had a small notebook he used to call his Guinness book of high school records where he used to record his results,  mostly maths.  They were ridiculous. Everything was just ridiculous apart from his weird books and science lessons. </p>
+          <p>His four years in high school believed to have lasted for several  centuries had finally come to an end and counter to what was said by almost everybody, each day was a slice of cake. Fortunately he got a chance to join a college and  it was going to be a walk in the park for him as he was to dig deeper into the  stuff that he enjoyed reading back in high school. This time most of it could be found in an exam. </p>
+          <p>He spent half of his entire college period in the library and the rest in the class,  church and sleeping. Immediately after college he realized that he had actually been drawing some dots in his past and that the line produced after joining them made a lot of sense. He found himself in an airport as an intern and that reminded him of his dreams about becoming a pilot. </p>
+          <p>After his internship he found himself implementing science with mathematics as a tool and realized that becoming an engineer was not as hard as he thought. At that point it was clear to him that nothing is usually as hard as it seems.  He also learnt the importance of having a dream and trusting in God because had it not been for that, his life would  have had no direction. </p>
+          <p>Today he’s an embedded systems designer, very passionate about technology and slowly venturing into the world of computer science with a view of complementing his career and eventually taking part in changing his little world. Wait! he just published his first post 🙂 </p>
       </div>
     </Layout>
   )
